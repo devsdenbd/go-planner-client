9403e64eb537f28a331600bd27dee49bd6f1c519
mdmmalamin <mdmmalamin@gmail.com>
2025-01-04 02:12:52 +0600

    Project Initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { MapLocationIcon } from "./assets/icons";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello Go Planner!</h1>;
+
+      <MapLocationIcon />
+    </div>
+  );
+}
+
+export default App;
